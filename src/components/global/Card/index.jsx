39365622c5f2280f2a51fe22b45c698f2b5d5f10
RasedulIslam <74731905+rasedul99@users.noreply.github.com>
2024-09-16 04:04:02 +0600

    initial commit

---
+++ src/components/global/Card/index.jsx
@@ -0,0 +1,38 @@
+import { Delete, Edit } from "@/components/icons";
+import { Checkbox } from "@/components/ui";
+import { cn } from "@/lib/utils";
+
+const Card = ({ todo, setTodos }) => {
+  const { id, title, completed } = todo;
+
+  const handleToggleComplete = (id) => {
+    setTodos((prev) => prev.map((todo) => todo.id !== id ? todo : {...todo, completed:!todo.completed} ))
+  }
+  const handleDelete = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div key={id} className="flex justify-between items-center gap-1">
+      <div className="grow flex items-center gap-2">
+        <Checkbox id={id} checked={completed} onClick = {()=> handleToggleComplete(id)}/>
+        <label
+          htmlFor={id}
+          className={cn(
+            `text-base font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70`,
+            completed && "line-through"
+          )}
+        >
+          {title}
+        </label>
+      </div>
+      <Edit className="size-5 cursor-pointer hover:text-red-600" />
+      <Delete
+        className="size-5 cursor-pointer hover:text-red-600"
+        onClick={() => handleDelete(id)}
+      />
+    </div>
+  );
+};
+
+export default Card;
---
+++ src/components/icons/index.js
@@ -0,0 +1,3 @@
+export {default as Delete} from "./Delete";
+export {default as Edit} from "./Edit";
+
